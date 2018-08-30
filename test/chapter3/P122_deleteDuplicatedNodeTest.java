118f502ff5b487a74112fee6b0c054da2f8ecc7e
deepsadness <a295756092@gmail.com>
2018-08-30 11:21:27 +0800

    commit P122

--- test/chapter3/P122_deleteDuplicatedNodeTest.java
+++ test/chapter3/P122_deleteDuplicatedNodeTest.java
@@ -7,6 +7,18 @@ import org.junit.Test;
 public class P122_deleteDuplicatedNodeTest {
 
     @Test
+    public void deleteDuplicatedNodeNo() {
+        ListNode nodeStart = ListNode.just(1);
+        nodeStart.next(2).next(3).next(4);
+        ListNode listNode = P122_deleteDuplicatedNode.deleteDuplicatedNode(nodeStart);
+
+        ListNode cmpNode = ListNode.just(1);
+        cmpNode.next(2).next(3).next(4);
+
+        Assert.assertEquals(cmpNode.toString(), listNode.toString());
+    }
+
+    @Test
     public void deleteDuplicatedNodeNormal() {
         ListNode nodeStart = ListNode.just(1);
         nodeStart.next(2).next(3).next(3);
@@ -34,10 +46,11 @@ public class P122_deleteDuplicatedNodeTest {
     @Test
     public void deleteDuplicatedNodeTwice() {
         ListNode nodeStart = ListNode.just(1);
-        nodeStart.next(1).next(2).next(3).next(3);
+        nodeStart.next(1).next(2).next(3).next(3).next(4);
         ListNode listNode = P122_deleteDuplicatedNode.deleteDuplicatedNode(nodeStart);
 
         ListNode cmpNode = ListNode.just(2);
+        cmpNode.next(4);
 
         Assert.assertEquals(cmpNode.toString(), listNode.toString());
     }
