a459a6f20b509b295ab3dd8673dc10098cf8a248
deepsadness <a295756092@gmail.com>
2018-09-04 01:24:40 +0800

    commit p205

--- src/chapter5/P205_MoreThanHalfNumber.java
+++ src/chapter5/P205_MoreThanHalfNumber.java
@@ -1,20 +1,19 @@
 package chapter5;
 
-import java.util.Arrays;
-
 /**
  * 数组中出现次数超过一半的数字
  * <p>
  * 找出数组中出现次数超过数组长度一半的数字。如输入{1,2,3,2,2,2,5,4,2}，则输出2。
  */
 public class P205_MoreThanHalfNumber {
-    public static void findMoreThanHalfNumber(int[] arr) {
+    //思路1 ：先排序，求中位数
+    public static int findMoreThanHalfNumber(int[] arr) {
         if (arr == null || arr.length < 3) {
-            return;
+            return 0;
         }
         //排序，中位数
-        quickSortRecursive(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        return arr[arr.length / 2];
     }
 
     private static void quickSort(int[] arr, int start, int end) {
@@ -22,63 +21,107 @@ public class P205_MoreThanHalfNumber {
             return;
         }
 
-        int base = arr[end];
         int left = start;
-        int right = end - 1;
+        int right = end;
+        int base = arr[left];
 
-        if (left < right) {
-            while (left < right && arr[left] < base) {
-                left++;
-            }
+        while (left < right) {
             while (right > left && arr[right] > base) {
                 right--;
             }
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
-        }
 
-        if (arr[left] > arr[right]) {
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
-        } else {
-            left++;
+            if (left < right) {
+                arr[left] = arr[right];
+                left++;
+            }
+
+            while (left < right && arr[left] < base) {
+                left++;
+            }
+
+            if (left < right) {
+                arr[right] = arr[left];
+                right--;
+            }
         }
+        arr[left] = base;
+
         quickSort(arr, start, left - 1);
         quickSort(arr, left + 1, end);
     }
 
-    private static void quickSortRecursive(int[] arr, int start, int end) {
-        if (start > end) {
-            return;
+    //思路2 使用快排分区 topK
+    // //转化为topK问题（此处求第k小的值），使用快排的分区函数解决，求第targetIndex+1小的数字（下标为targetIndex）
+    //书中说这种方法的时间复杂度为o(n)，但没懂为什么。网上也有人说为o(nlogk)
+    public static int findMoreThanHalfNumber2(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+        int left = 0, right = arr.length - 1;
+        //获取执行分区后下标为k的数据值（即第k+1小的数字）
+        int k = arr.length >>> 1;
+        int index = partition(arr, left, right);
+        while (index != k) {
+            if (index > k)
+                index = partition(arr, left, index - 1);
+            else
+                index = partition(arr, index + 1, right);
         }
-
-        int mid = arr[end];
-        int left = start;
-        int right = end - 1;
-
-        while (left < right) {
-            while (arr[left] < mid && left < right) {
-                left++;
-            }
-            while (arr[right] > mid && right > left) {
-                right--;
-            }
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
-        }
-
-        if (arr[left] > arr[right]) {
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
-        } else {
-            left++;
-        }
-        quickSortRecursive(arr, start, left - 1);
-        quickSortRecursive(arr, left + 1, end);
+        return arr[k];
     }
+
+    private static int partition(int[] arr, int left, int right) {
+        if (left < right) {
+            int start = left;
+            int end = right;
+            int base = arr[start];
+            while (start < end) {
+                while (start < end && arr[end] > base) {
+                    end--;
+                }
+
+                if (start < end) {
+                    arr[end] = arr[start];
+                    start++;
+                }
+
+                while (start < end && arr[start] < base) {
+                    start++;
+                }
+
+                if (start < end) {
+                    arr[start] = arr[end];
+                    end--;
+                }
+            }
+
+            arr[start] = base;
+
+            return start;
+        }
+        return left;
+    }
+
+    //思路3 缓存计数
+    public static int findMoreThanHalfNumber3(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+
+        int value = arr[0];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]) {
+                count++;
+            } else {
+                if (count > 0) {
+                    count--;
+                } else {
+                    value = arr[i];
+                }
+            }
+        }
+        return value;
+    }
+
 
 }
--- test/chapter5/P205_MoreThanHalfNumberTest.java
+++ test/chapter5/P205_MoreThanHalfNumberTest.java
@@ -1,5 +1,6 @@
 package chapter5;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class P205_MoreThanHalfNumberTest {
@@ -8,6 +9,22 @@ public class P205_MoreThanHalfNumberTest {
     public void findMoreThanHalfNumber() {
         int[] arr
                 = {1, 2, 3, 2, 2, 2, 5, 4, 2};
-        P205_MoreThanHalfNumber.findMoreThanHalfNumber(arr);
+        int halfNumber = P205_MoreThanHalfNumber.findMoreThanHalfNumber(arr);
+        Assert.assertEquals(2, halfNumber);
+    }
+
+    @Test
+    public void findMoreThanHalfNumber2() {
+        int[] arr
+                = {1, 2, 3, 2, 2, 2, 5, 4, 2};
+        int halfNumber = P205_MoreThanHalfNumber.findMoreThanHalfNumber2(arr);
+        Assert.assertEquals(2, halfNumber);
+    }
+    @Test
+    public void findMoreThanHalfNumber3() {
+        int[] arr
+                = {1, 2, 3, 2, 2, 2, 5, 4, 2};
+        int halfNumber = P205_MoreThanHalfNumber.findMoreThanHalfNumber3(arr);
+        Assert.assertEquals(2, halfNumber);
     }
 }
