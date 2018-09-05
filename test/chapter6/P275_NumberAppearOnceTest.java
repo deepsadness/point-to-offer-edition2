6ce80a47a668550baf92595331455b4fddcd81fd
deepsadness <a295756092@gmail.com>
2018-09-05 19:06:43 +0800

    commit P275

--- test/chapter6/P275_NumberAppearOnceTest.java
+++ test/chapter6/P275_NumberAppearOnceTest.java
@@ -1,5 +1,6 @@
 package chapter6;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,16 +9,16 @@ public class P275_NumberAppearOnceTest {
 
     @Test
     public void findFirst1Bit() {
-        System.out.println(P275_NumberAppearOnce.findFirst1Bit(0));
-        System.out.println(P275_NumberAppearOnce.findFirst1Bit(1));
-        System.out.println(P275_NumberAppearOnce.findFirst1Bit(2));
-        System.out.println(P275_NumberAppearOnce.findFirst1Bit(4));
-        System.out.println(P275_NumberAppearOnce.findFirst1Bit(6));
+        Assert.assertEquals(-1, P275_NumberAppearOnce.findFirst1Bit(0));
+        Assert.assertEquals(0, P275_NumberAppearOnce.findFirst1Bit(1));
+        Assert.assertEquals(1, P275_NumberAppearOnce.findFirst1Bit(2));
+        Assert.assertEquals(2, P275_NumberAppearOnce.findFirst1Bit(4));
+        Assert.assertEquals(1, P275_NumberAppearOnce.findFirst1Bit(6));
     }
 
     @Test
     public void onceNumber() {
         int[] arr = {2, 4, 3, 6, 3, 2, 5, 5};
-        System.out.println(Arrays.toString(P275_NumberAppearOnce.onceNumber(arr)));
+        Assert.assertEquals("[6, 4]",Arrays.toString(P275_NumberAppearOnce.onceNumber(arr)));
     }
 }
