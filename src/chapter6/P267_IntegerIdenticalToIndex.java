06fdae5db18d953634ea75776aadaa167d78cb3c
deepsadness <a295756092@gmail.com>
2018-09-05 16:14:13 +0800

    commit P267

--- src/chapter6/P267_IntegerIdenticalToIndex.java
+++ src/chapter6/P267_IntegerIdenticalToIndex.java
@@ -1,4 +1,30 @@
 package chapter6;
 
+/**
+ * 数组中数值和下标相等的元素
+ * <p>
+ * 假设一个单调递增的数组里的每个元素都是整数且是唯一的。
+ * 编写一个程序，找出数组中任意一个数值等于其下标的元素。例如，输入{-3,-1,1,3,5}，输出3。
+ */
 public class P267_IntegerIdenticalToIndex {
+    public static int findIndexEqualInteger(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] == mid) {
+                return mid;
+            } else if (arr[mid] < mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
---
+++ test/chapter6/P267_IntegerIdenticalToIndexTest.java
@@ -0,0 +1,14 @@
+package chapter6;
+
+import org.junit.Test;
+
+public class P267_IntegerIdenticalToIndexTest {
+
+    @Test
+    public void findIndexEqualInteger() {
+        System.out.println(P267_IntegerIdenticalToIndex.findIndexEqualInteger(new int[]{-3,-1,1,3,5})); //3
+        System.out.println(P267_IntegerIdenticalToIndex.findIndexEqualInteger(new int[]{0,1,2,3,4}));   //0~4
+        System.out.println(P267_IntegerIdenticalToIndex.findIndexEqualInteger(new int[]{4,5,6,7,8}));   //-1
+      
+    }
+}
