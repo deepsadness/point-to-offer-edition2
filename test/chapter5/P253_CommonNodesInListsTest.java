d168dc46624b262501bed87db5e4a811cd9e8da4
deepsadness <a295756092@gmail.com>
2018-09-05 14:40:26 +0800

    commit P253

--- test/chapter5/P253_CommonNodesInListsTest.java
+++ test/chapter5/P253_CommonNodesInListsTest.java
@@ -1,6 +1,7 @@
 package chapter5;
 
 import model.ListNode;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -10,15 +11,34 @@ import org.junit.Test;
 public class P253_CommonNodesInListsTest {
 
     @Test
-    public void findCommonNode1() {
+    public void findCommonNodeMid() {
         ListNode node1 = ListNode.just(1);
         ListNode node6 = node1.next(2).next(3).next(6);
         node6.next(7);
         ListNode node2 = ListNode.just(4);
         node2.next(5).next = node6;
         ListNode commonNode1 = P253_CommonNodesInLists.findCommonNode1(node1, node2);
+        Assert.assertEquals(commonNode1.value,6);
+    }
 
-        System.out.println(commonNode1 == null ? "NULL" : commonNode1.value);
+    @Test
+    public void findCommonNodeTail() {
+        ListNode node1 = ListNode.just(1);
+        ListNode node6 = node1.next(2).next(3).next(6);
+        ListNode node2 = ListNode.just(4);
+        node2.next(5).next = node6;
+        ListNode commonNode1 = P253_CommonNodesInLists.findCommonNode1(node1, node2);
+        Assert.assertEquals(commonNode1.value,6);
+    }
+
+    @Test
+    public void findCommonNodeNull() {
+        ListNode node1 = ListNode.just(1);
+        ListNode node6 = node1.next(2).next(3).next(6);
+        ListNode node2 = ListNode.just(4);
+        node2.next(5).next = node6;
+        ListNode commonNode1 = P253_CommonNodesInLists.findCommonNode1(node1, null);
+        Assert.assertNull(commonNode1);
     }
 
     @Test
@@ -29,6 +49,6 @@ public class P253_CommonNodesInListsTest {
         ListNode node2 = ListNode.just(4);
         node2.next(5).next = node6;
         ListNode commonNode1 = P253_CommonNodesInLists.findCommonNode2(node1, node2);
-        System.out.println(commonNode1.value);
+        Assert.assertEquals(commonNode1.value,6);
     }
 }
